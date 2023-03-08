c957307a1da9339ac234e9e85a32ffbc544a1ead
JianGodito <Jgodito27@gmail.com>
2023-03-08 08:32:23 -0800

    F

--- components/Experience.js
+++ components/Experience.js
@@ -1,5 +1,5 @@
-import { motion } from "framer-motion";
-import React from "react";
+import { motion } from "framer-motion"
+import React from "react"
 const Experience = () => {
   return (
     <>
@@ -9,68 +9,83 @@ const Experience = () => {
             Experience
           </h1>
         </div>
-        <motion.div className="grid max-w-xl grid-cols-1 pt-40 mx-auto"
-        initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 3, ease: "easeInOut" }} >
-        <div className="grid max-w-xl grid-cols-1 mx-auto ">
-          <div className="relative z-10 p-12 border rounded-lg -top-2 bg-darker dark:bg-lighter text-light dark:text-darker ">
-            <div className="">
-              <h2 className="absolute px-1 py-1 mx-auto bg-gray-500 rounded-lg -bottom-3 text-light">
-                December - February
-              </h2>
-            </div>
-              <h1 className="font-mono text-2xl text-start ">FrontEnd Developer</h1>
+        <motion.div
+          className="grid max-w-xl grid-cols-1 pt-40 mx-auto px-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 3, ease: "easeInOut" }}
+        >
+          <div className="grid max-w-xl grid-cols-1 mx-auto ">
+            <div className="relative z-10 p-12 border rounded-lg -top-2 bg-darker dark:bg-lighter text-light dark:text-darker ">
+              <div className="">
+                <h2 className="absolute px-1 py-1 mx-auto bg-gray-500 rounded-lg -bottom-3 text-light">
+                  December - February
+                </h2>
+              </div>
+              <h1 className="font-mono text-2xl text-start ">
+                FrontEnd Developer
+              </h1>
               <p className="py-4 text-4xl">VavaSoftware Corp.</p>
-            <ol className="list-disc">
-            <li>Built REST API that served data, PHP (Laravel) for the
-                backend, and VueJS for the front end. And Jenkins for
-                the testing, Build, and Deployment of the projects. And
-                JIRA for the scrums or tasks management, where tasks
-                log or mapped to customize the workflows. And
-                BITBUCKET is used to collaborate on code from concept
-                to Cloud, build quality code through automated testing,
-                and deploy code with confidence.</li>
-              <li>Maintain the quality of the projects, making sure that
-                  the project is easy to use and easy to understand.
-              </li>
-              <li>Update the User Interface, Fixing the bugs/Errors of the
+              <ol className="list-disc">
+                <li>
+                  Built REST API that served data, PHP (Laravel) for the
+                  backend, and VueJS for the front end. And Jenkins for the
+                  testing, Build, and Deployment of the projects. And JIRA for
+                  the scrums or tasks management, where tasks log or mapped to
+                  customize the workflows. And BITBUCKET is used to collaborate
+                  on code from concept to Cloud, build quality code through
+                  automated testing, and deploy code with confidence.
+                </li>
+                <li>
+                  Maintain the quality of the projects, making sure that the
+                  project is easy to use and easy to understand.
+                </li>
+                <li>
+                  Update the User Interface, Fixing the bugs/Errors of the
                   projects.
-              </li>
-              <li>Weekly Meetings about the progress of the projects.</li>
-            </ol>
+                </li>
+                <li>Weekly Meetings about the progress of the projects.</li>
+              </ol>
+            </div>
+            <div className="relative z-10 w-2 h-2 -mt-2 rounded-full bg-dark dark:bg-lighter animate-ping">
+              <div className="relative z-10 w-2 h-2 rounded-full bg-dark dark:bg-lighter animate-ping"></div>
+            </div>
+            <div className="w-1 h-24 -mt-3 bg-darker dark:bg-lighter"></div>
           </div>
-          <div className="relative z-10 w-2 h-2 -mt-2 rounded-full bg-dark dark:bg-lighter animate-ping">
-            <div className="relative z-10 w-2 h-2 rounded-full bg-dark dark:bg-lighter animate-ping"></div>
-          </div>
-          <div className="w-1 h-24 -mt-3 bg-darker dark:bg-lighter"></div>
-        </div>
           <div className="relative z-10 p-12 border rounded-lg bg-darker dark:bg-lighter text-light dark:text-darker ">
             <div>
               <h2 className="absolute px-1 py-1 mx-auto bg-gray-500 rounded-lg -bottom-3 text-light">
                 May - December
               </h2>
             </div>
-            <h1 className="font-mono text-2xl text-start ">FrontEnd Developer</h1>
+            <h1 className="font-mono text-2xl text-start ">
+              FrontEnd Developer
+            </h1>
             <p className="py-4 text-4xl">Innovations Group</p>
-           <ol className="list-disc">
-            <li>Built REST API that served data, Java for the backend,
-                and ReactJS for the front end. And Jenkins for the
-                testing, Build, and Deployment of the projects. And JIRA
-                for the scrum or tasks management, where tasks log or
-                mapped to customize the workflows</li>
-              <li>Maintain the quality of the projects, making sure that the
-                  project is easy to use and easy to understand
+            <ol className="list-disc">
+              <li>
+                Built REST API that served data, Java for the backend, and
+                ReactJS for the front end. And Jenkins for the testing, Build,
+                and Deployment of the projects. And JIRA for the scrum or tasks
+                management, where tasks log or mapped to customize the workflows
               </li>
-              <li>Update the User Interface, Fixing the bugs/Errors of the
-                  projects.
+              <li>
+                Maintain the quality of the projects, making sure that the
+                project is easy to use and easy to understand
               </li>
-              <li>Implementation, and Methodrification. Maintenance.
-                  System Testing and Deployment: Once you verify that
-                  the project is working.</li>
-              <li>Reviewed code and conducted testing for additional
-                  features.</li>
+              <li>
+                Update the User Interface, Fixing the bugs/Errors of the
+                projects.
+              </li>
+              <li>
+                Implementation, and Methodrification. Maintenance. System
+                Testing and Deployment: Once you verify that the project is
+                working.
+              </li>
+              <li>
+                Reviewed code and conducted testing for additional features.
+              </li>
               <li>Weekly Meetings about the progress of the projects.</li>
             </ol>
           </div>
@@ -78,50 +93,49 @@ const Experience = () => {
             <div className="relative z-10 w-2 h-2 rounded-full bg-dark dark:bg-lighter animate-ping"></div>
           </div>
           <div className="w-1 h-32 -mt-2 bg-darker dark:bg-lighter"></div>
+
+          {/* second card */}
+
+          <div className="grid max-w-xl grid-cols-1 mx-auto ">
+            <div className="relative z-10 p-12 border rounded-lg -top-2 bg-darker dark:bg-lighter text-light dark:text-darker ">
+              <div className="">
+                <h2 className="absolute px-1 py-1 mx-auto bg-gray-500 rounded-lg -bottom-3 text-light">
+                  January - April 2022
+                </h2>
+              </div>
+              <h1 className="font-mono text-2xl text-start ">OJT</h1>
+              <p className="py-4 underline">HUAWEI Technologies Co.</p>
+              <p>
+                I learned about the AWS or Amazon Web Services. And how it
+                works. AWS responsibility to managing the guest operating
+                system, And the MySQL engine on it.
+              </p>
+            </div>
+            <div className="relative z-10 w-2 h-2 -mt-2 rounded-full bg-dark dark:bg-lighter animate-ping">
+              <div className="relative z-10 w-2 h-2 rounded-full bg-dark dark:bg-lighter animate-ping"></div>
+            </div>
+            <div className="w-1 h-24 -mt-3 bg-darker dark:bg-lighter"></div>
+          </div>
+          <div className="grid max-w-xl grid-cols-1 mx-auto ">
+            <div className="relative z-10 p-12 border rounded-lg -top-2 bg-darker dark:bg-lighter text-light dark:text-darker ">
+              <div className="">
+                <h2 className="absolute px-1 py-1 mx-auto bg-gray-500 rounded-lg -bottom-3 text-light">
+                  July 2022
+                </h2>
+              </div>
+              <h1 className="font-mono text-2xl text-start ">Graduated</h1>
+              <p className="py-4">STI Laspinas</p>
+              <p>Bachelor of science in Information technology</p>
+            </div>
+            <div className="relative z-10 w-2 h-2 -mt-2 rounded-full bg-dark dark:bg-lighter animate-ping">
+              <div className="relative z-10 w-2 h-2 rounded-full bg-dark dark:bg-lighter animate-ping"></div>
+            </div>
+            <div className="w-1 h-24 -mt-3 bg-darker dark:bg-lighter"></div>
+          </div>
         </motion.div>
-
-        {/* second card */}
-
-        <div className="grid max-w-xl grid-cols-1 mx-auto ">
-          <div className="relative z-10 p-12 border rounded-lg -top-2 bg-darker dark:bg-lighter text-light dark:text-darker ">
-            <div className="">
-              <h2 className="absolute px-1 py-1 mx-auto bg-gray-500 rounded-lg -bottom-3 text-light">
-                January - April 2022
-              </h2>
-            </div>
-            <h1 className="font-mono text-2xl text-start ">OJT</h1>
-            <p className="py-4 underline">HUAWEI Technologies Co.</p>
-            <p>I learned about the AWS or Amazon Web Services. And how it works. AWS responsibility to managing the guest operating system, 
-            And the MySQL engine on it.</p>
-          </div>
-          <div className="relative z-10 w-2 h-2 -mt-2 rounded-full bg-dark dark:bg-lighter animate-ping">
-            <div className="relative z-10 w-2 h-2 rounded-full bg-dark dark:bg-lighter animate-ping"></div>
-          </div>
-          <div className="w-1 h-24 -mt-3 bg-darker dark:bg-lighter"></div>
-        </div>
-             <div className="grid max-w-xl grid-cols-1 mx-auto ">
-          <div className="relative z-10 p-12 border rounded-lg -top-2 bg-darker dark:bg-lighter text-light dark:text-darker ">
-            <div className="">
-              <h2 className="absolute px-1 py-1 mx-auto bg-gray-500 rounded-lg -bottom-3 text-light">
-               July 2022
-              </h2>
-            </div>
-            <h1 className="font-mono text-2xl text-start ">Graduated</h1>
-            <p className="py-4">STI Laspinas</p>
-            <p>Bachelor of science in Information technology</p>
-          </div>
-          <div className="relative z-10 w-2 h-2 -mt-2 rounded-full bg-dark dark:bg-lighter animate-ping">
-            <div className="relative z-10 w-2 h-2 rounded-full bg-dark dark:bg-lighter animate-ping"></div>
-          </div>
-          <div className="w-1 h-24 -mt-3 bg-darker dark:bg-lighter"></div>
-        </div>
-        
       </section>
-
-
-
     </>
-  );
-};
+  )
+}
 
-export default Experience;
+export default Experience
--- components/Hero.js
+++ components/Hero.js
@@ -1,31 +1,33 @@
-import { motion } from "framer-motion";
-import React from "react";
-import { IoIosChatbubbles } from "react-icons/io";
-import { MdDownload } from 'react-icons/md';
-import About from "./About";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import { motion } from "framer-motion"
+import React from "react"
+import { IoIosChatbubbles } from "react-icons/io"
+import { MdDownload } from "react-icons/md"
+import About from "./About"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
 
-const Hero = ( {children}) => {
+const Hero = ({ children }) => {
   return (
-    <div
-    >
+    <div>
       <Navbar />
       <div className="h-full bg-lighter dark:bg-darker md:h-full">
-        <motion.div className="container items-center max-w-6xl mx-auto xl:px-5 mb-52 sm:mb-auto"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 2, ease: "easeInOut" }} >
+        <motion.div
+          className="container items-center max-w-6xl mx-auto xl:px-5 mb-52 sm:mb-auto"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+        >
           <div className="flex flex-wrap items-center sm:-mx-3">
             <div className="w-full md:w-1/2 md:px-3">
-              <div className="w-full text-center md:text-left pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-10 md:pb-0">
+              <div className="w-full text-center md:text-left pb-2 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-10 md:pb-0">
                 <motion.h1 className="-mb-6 text-4xl text-center md:text-left md:text-5xl text-gray-900 sm:text-4xl  dark:text-gray-200">
-                  Hi, I'm  <span className="w-10 h-20 pt-2 font-mono font-bold animate-wiggle whitespace-nowrap text-brand-accent">
+                  Hi, I'm{" "}
+                  <span className="w-10 h-20 pt-2 font-mono font-bold animate-wiggle whitespace-nowrap text-brand-accent">
                     Jian Godito
                   </span>
                 </motion.h1>
-                 <p className="mx-auto text-sm text-gray-700 sm:text-base dark:text-gray-200 sm:max-w-md lg:text-xl md:max-w-3xl">
+                <p className="mx-auto text-sm px-12 text-gray-700 sm:text-base dark:text-gray-200 sm:max-w-md lg:text-xl md:max-w-3xl">
                   A 23 year old{" "}
                   <span className="font-bold ">Front-end Developer </span>
                   based in Laspinas, Philippines.
@@ -33,17 +35,21 @@ const Hero = ( {children}) => {
                 <div className="flex justify-center md:justify-start">
                   <button className="px-3 w-38 py-2 mr-2 text-center rounded-full dark:bg-lighter bg-darker hover:opacity-75 hover:dark:opacity-25 dark:text-darker text-lighter ">
                     <a
-                        href="/files/JMGResumeV2.pdf"
-                        alt="alt text"
-                        target="_blank"
+                      href="/files/JMGResumeV2.pdf"
+                      alt="alt text"
+                      target="_blank"
                       rel="noopener noreferrer"
                       className="flex my-auto font-quick text-sm font-bold"
-                    >Download CV <MdDownload className="flex m-auto ml-1" />
+                    >
+                      Download CV <MdDownload className="flex m-auto ml-1" />
                     </a>
                   </button>
-                  
+
                   <button className="px-3 py-1 rounded-full text-center dark:bg-lighter bg-darker hover:opacity-75 hover:dark:opacity-25 dark:text-darker text-lighter  ">
-                    <a href="/contact" className="flex my-auto font-quick text-sm font-bold">
+                    <a
+                      href="/contact"
+                      className="flex my-auto font-quick text-sm font-bold"
+                    >
                       Contact Me
                       <IoIosChatbubbles className="flex my-auto ml-1" />
                     </a>
@@ -68,7 +74,7 @@ const Hero = ( {children}) => {
         <Footer />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
